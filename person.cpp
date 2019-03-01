d255c45da7ff983984e7e1af59272e6a9500ef8d
cjerry80 <cjerry80@cs3006b.acad.fullerton.edu>
2019-02-28 17:02:05 -0800

    Modified person

--- person.cpp
+++ person.cpp
@@ -6,26 +6,30 @@ using namespace std;
 
 
 Person::Person(){
+string firstn;
+string lastn;
+float taxR;
+float hoursW;
 
 }
 string Person::getLastName(){
 return lastName;
 }
 void Person::setFirstName(string fName){
-
+firstn = fName;
 }
 string Person::getFirstName(){
 return firstName;
 }
 void Person::setPayRate(float rate){
-
+taxR = rate;
 }
 float Person::getPayRate(){
 return payRate;
 
 }
 void Person::setHoursWorked(float hours){
-
+hoursW = hours;
 }
 float Person::getHoursWorked(){
 return hoursWorked;
