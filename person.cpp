a83bef6637159f01a7446ebe333aec2faa082a28
cjerry80 <cjerry80@cs3006b.acad.fullerton.edu>
2019-02-28 16:29:24 -0800

    modified person

--- person.cpp
+++ person.cpp
@@ -1,1 +1,38 @@
 #include "person.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+Person::Person(){
+
+}
+string Person::getLastName(){
+return lastName;
+}
+void Person::setFirstName(string fName){
+
+}
+string Person::getFirstName(){
+return firstName;
+}
+void Person::setPayRate(float rate){
+
+}
+float Person::getPayRate(){
+return payRate;
+
+}
+void Person::setHoursWorked(float hours){
+
+}
+float Person::getHoursWorked(){
+return hoursWorked;
+}
+float Person::totalPay(){
+
+}
+string Person::fullName(){
+
+}
